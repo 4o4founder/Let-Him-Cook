b3875e896075a1e5063019ebd11cd1d67a473549
4o4founder <royalbullet1239@gmail.com>
2026-05-18 21:19:34 +0530

    Create api.js

---
+++ frontend/src/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+
+const API = axios.create({ baseURL: 'https://let-him-cook-backend-kk1k.onrender.com' });
+
+
+API.interceptors.request.use((req) => {
+    if (localStorage.getItem('profile')) {
+        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+    }
+    return req;
+});
+
+export default API;
